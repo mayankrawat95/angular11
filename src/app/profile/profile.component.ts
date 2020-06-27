a2f8388127706ab57e96f2d0193abc115fdebd1b
Mayank Rawat <mayankrawat95@gmail.com>
2020-06-27 13:12:33 +0530

    hi

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,12 +11,13 @@ import { ProfserviceService } from './profservice.service';
 })
 export class ProfileComponent implements OnInit {
 
-  @ViewChild('f', {static: false}) Form: NgForm
+  @ViewChild('f', {static: false}) form: NgForm
   name1: string;
   DOB1: string;
  email1: string;
  gender1: string;
  profile: Profile;
+
 
   FormVisibility = false;
 
@@ -30,14 +31,37 @@ export class ProfileComponent implements OnInit {
    this.name1 = this.profileService.profile.name;
    this.DOB1 = this.profileService.profile.DOB;
    this.email1 = this.profileService.profile.Email;
-   this.gender1 = this.profileService.profile.Gender;
+   this.gender1 = this.profileService.profile.Gender; 
+   
   }
 
-
+  
 
   onSubmit(form: NgForm){
 
-  this.profileService.profile.name = this.name1;
+    this.name1 = form.value.name;
+    
+    this.DOB1 = form.value.DOB;
+    this.email1 = form.value.email;
+    this.gender1 = form.value.gender;
+
+    this.profileService.profile.name = this.name1;
+  this.profileService.profile.DOB = this.DOB1;
+  this.profileService.profile.Email = this.email1;
+   this.profileService.profile.Gender = this.gender1
+
+    const newProfile =  new Profile(
+      this.name1,
+      this.DOB1 ,
+      this.email1 ,
+       this.gender1
+     )
+     
+
+     this.profileService.updateProfile(newProfile);
+
+
+ /* this.profileService.profile.name = this.name1;
   this.profileService.profile.DOB = this.DOB1;
   this.profileService.profile.Email = this.email1;
    this.profileService.profile.Gender = this.gender1 ;
@@ -49,19 +73,22 @@ export class ProfileComponent implements OnInit {
       this.gender1
     )
 
-    this.profileService.updateProfile(newProfile);
+    this.profileService.updateProfile(newProfile); */
 
     this.FormVisibility = false;
+    
    
   }
 
-      updateProfile(newProfile){
+   /* updateProfile(newProfile){
      newProfile = new Profile(this.name1, this.DOB1, this.email1, this.gender1);
       return newProfile.slice();
-      }
+      }*/ 
 
-  showForm(){
+  showForm(form: NgForm){
     this.FormVisibility = true;
+   
+    
   }
 
 }
